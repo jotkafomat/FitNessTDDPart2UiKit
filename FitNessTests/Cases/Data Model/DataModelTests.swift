7826af3a621c00b76d7b386bd2f0f1a23a1c18ed
jotkafomat <k-j@xl.wp.pl>
2021-06-24 16:13:10 +0100

    Initial Commit

---
+++ FitNessTests/Cases/Data Model/DataModelTests.swift
@@ -0,0 +1,21 @@
+//
+//  DataModelTests.swift
+//  FitNessTests
+//
+//  Created by Krzysztof Jankowski on 24/06/2021.
+//  Copyright © 2021 Razeware. All rights reserved.
+//
+
+import XCTest
+
+class DataModelTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
